f24e84aa0256a7ec6e35f5acceb616f206f77642
Michael Friedrich <michael.friedrich@univie.ac.at>
2009-08-29 02:18:21 +0200

    query refactoring insert or update finished queries in dbqueries.c experimental code for postgres

--- module/idoutils/include/dbqueries.h
+++ module/idoutils/include/dbqueries.h
@@ -52,4 +52,69 @@ int ido2db_query_insert_or_update_hoststatusdata_add(ndo2db_idi *idi, void **dat
 /* SERVICESTATUS */
 int ido2db_query_insert_or_update_servicestatusdata_add(ndo2db_idi *idi, void **data);
 
+/* CONTACTSTATUS */
+int ido2db_query_insert_or_update_contactstatusdata_add(ndo2db_idi *idi, void **data);
+
+/* CONFIGFILEVARIABLES */
+int ido2db_query_insert_or_update_configfilevariables_add(ndo2db_idi *idi, void **data);
+
+/* RUNTIMEVARIABLES */
+int ido2db_query_insert_or_update_runtimevariables_add(ndo2db_idi *idi, void **data);
+
+/* HOSTDEFINITION */
+int ido2db_query_insert_or_update_hostdefinition_definition_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_hostdefinition_parenthosts_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_hostdefinition_contactgroups_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_hostdefinition_contacts_add(ndo2db_idi *idi, void **data);
+
+/* HOSTGROUPDEFINITION */
+int ido2db_query_insert_or_update_hostgroupdefinition_definition_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_hostgroupdefinition_hostgroupmembers_add(ndo2db_idi *idi, void **data);
+
+/* SERVICEDEFINITION */
+int ido2db_query_insert_or_update_servicedefinition_definition_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_servicedefinition_contactgroups_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_servicedefinition_contacts_add(ndo2db_idi *idi, void **data);
+
+/* SERVICEGROUPDEFINITION */
+int ido2db_query_insert_or_update_servicegroupdefinition_definition_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_servicegroupdefinition_members_add(ndo2db_idi *idi, void **data);
+
+/* HOSTDEPENDENCYDEFINITION */
+int ido2db_query_insert_or_update_hostdependencydefinition_definition_add(ndo2db_idi *idi, void **data);
+
+/* SERVICEDEPENDENCYDEFINITION */
+int ido2db_query_insert_or_update_servicedependencydefinition_definition_add(ndo2db_idi *idi, void **data);
+
+/* HOSTESCALATIONDEFINITION */
+int ido2db_query_insert_or_update_hostescalationdefinition_definition_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_hostescalationdefinition_contactgroups_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_hostescalationdefinition_contacts_add(ndo2db_idi *idi, void **data);
+
+/* SERVICEESCALATIONDEFINITION */
+int ido2db_query_insert_or_update_serviceescalationdefinition_definition_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_serviceescalationdefinition_contactgroups_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_serviceescalationdefinition_contacts_add(ndo2db_idi *idi, void **data);
+
+/*  COMMANDDEFINITION */
+int ido2db_query_insert_or_update_commanddefinition_definition_add(ndo2db_idi *idi, void **data);
+
+/*  TIMEPERIODDEFINITION */
+int ido2db_query_insert_or_update_timeperiodefinition_definition_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_timeperiodefinition_timeranges_add(ndo2db_idi *idi, void **data);
+
+/* CONTACTDEFINITION */
+int ido2db_query_insert_or_update_contactdefinition_definition_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_contactdefinition_addresses_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_contactdefinition_hostnotificationcommands_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_contactdefinition_servicenotificationcommands_add(ndo2db_idi *idi, void **data);
+
+/* CUSTOMVARIABLES */
+int ido2db_query_insert_or_update_save_custom_variables_customvariables_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_save_custom_variables_customvariablestatus_add(ndo2db_idi *idi, void **data);
+
+/* CONTACTGROUPDEFINITION */
+int ido2db_query_insert_or_update_contactgroupdefinition_definition_add(ndo2db_idi *idi, void **data);
+int ido2db_query_insert_or_update_contactgroupdefinition_contactgroupmembers_add(ndo2db_idi *idi, void **data);
+
 #endif
