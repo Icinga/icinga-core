f979dbdc1b57ba541a329bf08dca63737ef4e555
Michael Friedrich <michael.friedrich@univie.ac.at>
2009-09-05 18:03:48 +0200

    deleted old mysql files

--- module/idoutils/db/mysql-upgrade-1.4b8.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `icinga`.`icinga_hostchecks` MODIFY COLUMN `perfdata` VARCHAR(8192) NULL;
-ALTER TABLE `icinga`.`icinga_hoststatus` MODIFY COLUMN `perfdata` VARCHAR(8192) NULL;
-ALTER TABLE `icinga`.`icinga_servicechecks` MODIFY COLUMN `perfdata` VARCHAR(8192) NULL;
-ALTER TABLE `icinga`.`icinga_servicestatus` MODIFY COLUMN `perfdata` VARCHAR(8192) NULL;
-
