935ee9dae05bd6c945b0e66a098b7d2fef23fae5
Hendrik Baecker <andurin@process-zero.de>
2009-09-14 08:10:11 +0200

    Revert "Added SSL Support to IDOUtils"
    This reverts commit 866b906e9dfda249923727ef8929090b050b1ac9.
    
    Conflicts:
    
    	Changelog
    	module/idoutils/src/ido2db.c
    
    The native NDOUtils SSL patch was designed to match to the complete
    compile process. But this caused some ugly segfaults in icinga.
    The SSL feature was moved to a debug branch so this can be reverted
    in master.

--- module/idoutils/src/io.c
+++ module/idoutils/src/io.c
@@ -11,14 +11,6 @@
 #include "../include/common.h"
 #include "../include/io.h"
 
-#ifdef HAVE_SSL
-SSL_METHOD *meth;
-SSL_CTX *ctx;
-SSL *ssl;
-int use_ssl=NDO_TRUE;
-#else
-int use_ssl=NDO_FALSE;
-#endif
 
 
 
@@ -148,7 +140,6 @@ int ndo_sink_open(char *name, int fd, int type, int port, int flags, int *nfd){
 	struct hostent *hp=NULL;
 	mode_t mode=S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP;
 	int newfd=0;
-	int rc=0;
 
 	/* use file */
 	if(type==NDO_SINK_FILE){
@@ -191,23 +182,6 @@ int ndo_sink_open(char *name, int fd, int type, int port, int flags, int *nfd){
 		if(name==NULL)
 			return NDO_ERROR;
 		
-#ifdef HAVE_SSL
-		if(use_ssl==NDO_TRUE){
-			SSL_library_init();
-			SSLeay_add_ssl_algorithms();
-			meth=SSLv23_client_method();
-			SSL_load_error_strings();
-
-			if((ctx=SSL_CTX_new(meth))==NULL){
-					printf("CHECK_NRPE: Error - could not create SSL context.\n");
-					return NDO_ERROR;
-			}
-			/* ADDED 01/19/2004 */
-			/* use only TLSv1 protocol */
-			SSL_CTX_set_options(ctx,SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3);
-		}
-#endif
-
 		/* clear the address */
 		bzero((char *)&server_address_i,sizeof(server_address_i));
 
@@ -234,25 +208,7 @@ int ndo_sink_open(char *name, int fd, int type, int port, int flags, int *nfd){
 			close(newfd);
 			return NDO_ERROR;
 		        }
-
-#ifdef HAVE_SSL
-		if(use_ssl==NDO_TRUE){
-			if((ssl=SSL_new(ctx))!=NULL){
-				SSL_CTX_set_cipher_list(ctx,"ADH");
-				SSL_set_fd(ssl,newfd);
-				if((rc=SSL_connect(ssl))!=1){
-					printf("Error - Could not complete SSL handshake.\n");
-					SSL_CTX_free(ctx);
-					close(newfd);
-					return NDO_ERROR;
-				}
-			} else {
-				printf("CHECK_NRPE: Error - Could not create SSL connection structure.\n");
-				return NDO_ERROR;
-			}
-		}
-#endif
-		}
+	        }
 
 	/* unknown sink type */
 	else
@@ -278,12 +234,7 @@ int ndo_sink_write(int fd, char *buf, int buflen){
 	while(tbytes<buflen){
 
 		/* try to write everything we have left */
-#ifdef HAVE_SSL
-		if (use_ssl == NDO_TRUE)
-			result=SSL_write(ssl, buf+tbytes, buflen-tbytes);
-		else
-#endif
-			result=write(fd, buf+tbytes, buflen-tbytes);
+		result=write(fd,buf+tbytes,buflen-tbytes);
 
 		/* some kind of error occurred */
 		if(result==-1){
