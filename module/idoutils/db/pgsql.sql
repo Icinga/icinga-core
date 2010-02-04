bfbb8b88cd2fac1d4f6205ecae7a4ce981af85d3
Michael Friedrich <michael.friedrich@univie.ac.at>
2010-02-04 12:59:13 +0100

    modify index names for pgsql/oracle
    mysql takes index names for table, pgsql/oracle need unique names

--- module/idoutils/db/pgsql.sql
+++ module/idoutils/db/pgsql.sql
@@ -1397,63 +1397,65 @@ CREATE TABLE  icinga_timeperiod_timeranges (
 -- EXTERNALCOMMANDS => entry_time
 
 -- instance_id
-CREATE INDEX instance_id_idx on icinga_timedevents(instance_id);
-CREATE INDEX instance_id_idx on icinga_systemcommands(instance_id);
-CREATE INDEX instance_id_idx on icinga_servicechecks(instance_id);
-CREATE INDEX instance_id_idx on icinga_hostchecks(instance_id);
-CREATE INDEX instance_id_idx on icinga_eventhandlers(instance_id);
-CREATE INDEX instance_id_idx on icinga_externalcommands(instance_id);
+CREATE INDEX timedevents_i_id_idx on icinga_timedevents(instance_id);
+CREATE INDEX systemcommands_i_id_idx on icinga_systemcommands(instance_id);
+CREATE INDEX servicechecks_i_id_idx on icinga_servicechecks(instance_id);
+CREATE INDEX hostchecks_i_id_idx on icinga_hostchecks(instance_id);
+CREATE INDEX eventhandlers_i_id_idx on icinga_eventhandlers(instance_id);
+CREATE INDEX externalcommands_i_id_idx on icinga_externalcommands(instance_id);
 
 -- time
-CREATE INDEX time_id_idx on icinga_timedevents(scheduled_time);
-CREATE INDEX time_id_idx on icinga_systemcommands(start_time);
-CREATE INDEX time_id_idx on icinga_servicechecks(start_time);
-CREATE INDEX time_id_idx on icinga_hostchecks(start_time);
-CREATE INDEX time_id_idx on icinga_eventhandlers(start_time);
-CREATE INDEX time_id_idx on icinga_externalcommands(entry_time);
+CREATE INDEX timedevents_time_id_idx on icinga_timedevents(scheduled_time);
+CREATE INDEX systemcommands_time_id_idx on icinga_systemcommands(start_time);
+CREATE INDEX servicechecks_time_id_idx on icinga_servicechecks(start_time);
+CREATE INDEX hostchecks_time_id_idx on icinga_hostchecks(start_time);
+CREATE INDEX eventhandlers_time_id_idx on icinga_eventhandlers(start_time);
+CREATE INDEX externalcommands_time_id_idx on icinga_externalcommands(entry_time);
+
 
 -- for starting cleanup - referenced in dbhandler.c:882
 -- instance_id only
 
 -- realtime data
-CREATE INDEX instance_id_idx on icinga_programstatus(instance_id);
-CREATE INDEX instance_id_idx on icinga_hoststatus(instance_id);
-CREATE INDEX instance_id_idx on icinga_servicestatus(instance_id);
-CREATE INDEX instance_id_idx on icinga_contactstatus(instance_id);
-CREATE INDEX instance_id_idx on icinga_timedeventqueue(instance_id);
-CREATE INDEX instance_id_idx on icinga_comments(instance_id);
-CREATE INDEX instance_id_idx on icinga_scheduleddowntime(instance_id);
-CREATE INDEX instance_id_idx on icinga_runtimevariables(instance_id);
-CREATE INDEX instance_id_idx on icinga_customvariablestatus(instance_id);
+CREATE INDEX programstatus_i_id_idx on icinga_programstatus(instance_id);
+CREATE INDEX hoststatus_i_id_idx on icinga_hoststatus(instance_id);
+CREATE INDEX servicestatus_i_id_idx on icinga_servicestatus(instance_id);
+CREATE INDEX contactstatus_i_id_idx on icinga_contactstatus(instance_id);
+CREATE INDEX timedeventqueue_i_id_idx on icinga_timedeventqueue(instance_id);
+CREATE INDEX comments_i_id_idx on icinga_comments(instance_id);
+CREATE INDEX scheduleddowntime_i_id_idx on icinga_scheduleddowntime(instance_id);
+CREATE INDEX runtimevariables_i_id_idx on icinga_runtimevariables(instance_id);
+CREATE INDEX customvariablestatus_i_id_idx on icinga_customvariablestatus(instance_id);
 
 -- config data
-CREATE INDEX instance_id_idx on icinga_configfiles(instance_id);
-CREATE INDEX instance_id_idx on icinga_configfilevariables(instance_id);
-CREATE INDEX instance_id_idx on icinga_customvariables(instance_id);
-CREATE INDEX instance_id_idx on icinga_commands(instance_id);
-CREATE INDEX instance_id_idx on icinga_timeperiods(instance_id);
-CREATE INDEX instance_id_idx on icinga_timeperiod_timeranges(instance_id);
-CREATE INDEX instance_id_idx on icinga_contactgroups(instance_id);
-CREATE INDEX instance_id_idx on icinga_contactgroup_members(instance_id);
-CREATE INDEX instance_id_idx on icinga_hostgroups(instance_id);
-CREATE INDEX instance_id_idx on icinga_hostgroup_members(instance_id);
-CREATE INDEX instance_id_idx on icinga_servicegroups(instance_id);
-CREATE INDEX instance_id_idx on icinga_servicegroup_members(instance_id);
-CREATE INDEX instance_id_idx on icinga_hostescalations(instance_id);
-CREATE INDEX instance_id_idx on icinga_hostescalation_contacts(instance_id);
-CREATE INDEX instance_id_idx on icinga_serviceescalations(instance_id);
-CREATE INDEX instance_id_idx on icinga_serviceescalation_contacts(instance_id);
-CREATE INDEX instance_id_idx on icinga_hostdependencies(instance_id);
-CREATE INDEX instance_id_idx on icinga_contacts(instance_id);
-CREATE INDEX instance_id_idx on icinga_contact_addresses(instance_id);
-CREATE INDEX instance_id_idx on icinga_contact_notificationcommands(instance_id);
-CREATE INDEX instance_id_idx on icinga_hosts(instance_id);
-CREATE INDEX instance_id_idx on icinga_host_parenthosts(instance_id);
-CREATE INDEX instance_id_idx on icinga_host_contacts(instance_id);
-CREATE INDEX instance_id_idx on icinga_services(instance_id);
-CREATE INDEX instance_id_idx on icinga_service_contacts(instance_id);
-CREATE INDEX instance_id_idx on icinga_service_contactgroups(instance_id);
-CREATE INDEX instance_id_idx on icinga_host_contactgroups(instance_id);
-CREATE INDEX instance_id_idx on icinga_hostescalation_contactgroups(instance_id);
-CREATE INDEX instance_id_idx on icinga_serviceescalation_contactgroups(instance_id);
+CREATE INDEX configfiles_i_id_idx on icinga_configfiles(instance_id);
+CREATE INDEX configfilevariables_i_id_idx on icinga_configfilevariables(instance_id);
+CREATE INDEX customvariables_i_id_idx on icinga_customvariables(instance_id);
+CREATE INDEX commands_i_id_idx on icinga_commands(instance_id);
+CREATE INDEX timeperiods_i_id_idx on icinga_timeperiods(instance_id);
+CREATE INDEX timeperiod_timeranges_i_id_idx on icinga_timeperiod_timeranges(instance_id);
+CREATE INDEX contactgroups_i_id_idx on icinga_contactgroups(instance_id);
+CREATE INDEX contactgroup_members_i_id_idx on icinga_contactgroup_members(instance_id);
+CREATE INDEX hostgroups_i_id_idx on icinga_hostgroups(instance_id);
+CREATE INDEX hostgroup_members_i_id_idx on icinga_hostgroup_members(instance_id);
+CREATE INDEX servicegroups_i_id_idx on icinga_servicegroups(instance_id);
+CREATE INDEX servicegroup_members_i_id_idx on icinga_servicegroup_members(instance_id);
+CREATE INDEX hostesc_i_id_idx on icinga_hostescalations(instance_id);
+CREATE INDEX hostesc_contacts_i_id_idx on icinga_hostescalation_contacts(instance_id);
+CREATE INDEX serviceesc_i_id_idx on icinga_serviceescalations(instance_id);
+CREATE INDEX serviceesc_contacts_i_id_idx on icinga_serviceescalation_contacts(instance_id);
+CREATE INDEX hostdependencies_i_id_idx on icinga_hostdependencies(instance_id);
+CREATE INDEX contacts_i_id_idx on icinga_contacts(instance_id);
+CREATE INDEX contact_addresses_i_id_idx on icinga_contact_addresses(instance_id);
+CREATE INDEX contact_notifcommands_i_id_idx on icinga_contact_notificationcommands(instance_id);
+CREATE INDEX hosts_i_id_idx on icinga_hosts(instance_id);
+CREATE INDEX host_parenthosts_i_id_idx on icinga_host_parenthosts(instance_id);
+CREATE INDEX host_contacts_i_id_idx on icinga_host_contacts(instance_id);
+CREATE INDEX services_i_id_idx on icinga_services(instance_id);
+CREATE INDEX service_contacts_i_id_idx on icinga_service_contacts(instance_id);
+CREATE INDEX service_contactgroups_i_id_idx on icinga_service_contactgroups(instance_id);
+CREATE INDEX host_contactgroups_i_id_idx on icinga_host_contactgroups(instance_id);
+CREATE INDEX hostesc_cgroups_i_id_idx on icinga_hostescalation_contactgroups(instance_id);
+CREATE INDEX serviceesc_cgroups_i_id_idx on icinga_serviceescalation_contactgroups(instance_id);
+
 
