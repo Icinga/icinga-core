f8ee80103deab47dc450b954aa7207feec7d879d
Michael Friedrich <michael.friedrich@univie.ac.at>
2010-05-11 18:31:00 +0200

    introduce common/shared.c and include/shared.h (Andreas Ericsson)
    [--snip--]
    A lot of functionality was previously duplicate between base/utils.c
    and cgi/cgiutils.c, but with some minor code-rot between the various
    versions, depending on when bugs were found where. This consolidates
    the two files into a new one, which is compile-state independent and
    thus doesn't require re-compilation between compiling cgis and
    Nagios.
    
    Minor bugs that were previously fixed in either the daemon or the
    cgi's are now fixed in both (assuming my manual code analysis is
    correct anyways).
    
    This change was long overdue. Now we just need to keep duplicate
    code from entering the repository again.
    [--snip--]
    
    slightly modified to fit into Icinga.
    
    fixes #389

--- include/common.h
+++ include/common.h
@@ -22,6 +22,7 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************/
 
+#include "shared.h"
 
 #define PROGRAM_NAME "Icinga"
 #define PROGRAM_NAME_UC "ICINGA"
