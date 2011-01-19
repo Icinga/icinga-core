d8be7c8476dd93694578a662cdd5ed96e16be5ef
Michael Friedrich <michael.friedrich@univie.ac.at>
2011-01-19 14:57:15 +0100

    idoutils: do not clear host|service status tables on core restart/reload (inspired by Opsview) #1106
    fixes #1106

--- module/idoutils/src/dbhandlers.c
+++ module/idoutils/src/dbhandlers.c
@@ -1323,9 +1323,11 @@ int ido2db_handle_processdata(ido2db_idi *idi) {
 		if(ido2db_db_settings.clean_realtime_tables_on_core_startup==IDO_TRUE){ /* only if desired */
 
 			/* clear realtime data */
+			/* don't clear necessary status tables on restart/reload of the core, as Icinga Web
+			   won't show any data then */
 			ido2db_db_clear_table(idi, ido2db_db_tablenames[IDO2DB_DBTABLE_PROGRAMSTATUS]);
-			ido2db_db_clear_table(idi, ido2db_db_tablenames[IDO2DB_DBTABLE_HOSTSTATUS]);
-			ido2db_db_clear_table(idi, ido2db_db_tablenames[IDO2DB_DBTABLE_SERVICESTATUS]);
+			/* ido2db_db_clear_table(idi, ido2db_db_tablenames[IDO2DB_DBTABLE_HOSTSTATUS]); */
+			/* ido2db_db_clear_table(idi, ido2db_db_tablenames[IDO2DB_DBTABLE_SERVICESTATUS]); */
 			ido2db_db_clear_table(idi, ido2db_db_tablenames[IDO2DB_DBTABLE_CONTACTSTATUS]);
 			ido2db_db_clear_table(idi, ido2db_db_tablenames[IDO2DB_DBTABLE_TIMEDEVENTQUEUE]);
 			ido2db_db_clear_table(idi, ido2db_db_tablenames[IDO2DB_DBTABLE_COMMENTS]);
