dea984dbf01118ccef6d868f10d18c7b1414ea30
Ricardo Bartels <ricardo@bitchbrothers.com>
2011-03-08 11:00:13 +0100

    cmd.cgi: acknowledgement multiline comment -> command not being processed #1257
    Fixes #1257

--- cgi/cmd.c
+++ cgi/cmd.c
@@ -2667,7 +2667,7 @@ void clean_comment_data(char *buffer){
 	y=(int)strlen(buffer);
 
 	for(x=0;x<y;x++){
-		if(buffer[x]==';' || buffer[x]=='\n')
+		if(buffer[x]==';' || buffer[x]=='\n' || buffer[x]=='\r' )
 			buffer[x]=' ';
 	}
 
