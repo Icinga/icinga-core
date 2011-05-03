ed19196853eb2738be6cf605ecdaedfcb2dc4f72
Thomas Dressler <tdressler@tdressler.net>
2011-05-03 23:08:39 +0200

    includes all fixes from upgrade refs #1401,#1173,#1355

--- module/idoutils/db/pgsql/upgrade/pgsql-upgrade-1.4.0.sql
+++ module/idoutils/db/pgsql/upgrade/pgsql-upgrade-1.4.0.sql
@@ -8,15 +8,18 @@
 -- -----------------------------------------
 
 -- -----------------------------------------
+-- remove number limitations, change serial to bigserial type
+-- fixes Bug #1173: int(11) to small for some of ido tables https://dev.icinga.org/issues/1173
+--       Bug #1401: integer not big enough for bytes_processed https://dev.icinga.org/issues/1401
+-- remove NOT NULL constraints
+-- Feature #1355 drop unnessary constraints and rename remaining https://dev.icinga.org/issues/1355
+-- -----------------------------------------
+\i pgsql_alter_icinga13_types.sql
+
+-- -----------------------------------------
 -- update dbversion
 -- -----------------------------------------
 
 SELECT updatedbversion('1.4.0');
 
--- -----------------------------------------
--- change integer to bigint
--- -----------------------------------------
-
-ALTER TABLE icinga_conninfo ALTER COLUMN bytes_processed TYPE BIGINT;
-ALTER TABLE icinga_conninfo ALTER COLUMN lines_processed TYPE BIGINT;
 
