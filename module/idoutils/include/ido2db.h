a7f6b37a666874afaa54c2f93c25b9287c2480d0
Michael Friedrich <michael.friedrich@univie.ac.at>
2011-08-09 18:08:54 +0200

    now flag this version pre merge for final target release date

--- module/idoutils/include/ido2db.h
+++ module/idoutils/include/ido2db.h
@@ -13,7 +13,7 @@
 #include "utils.h"
 
 #define IDO2DB_NAME "IDO2DB"
-#define IDO2DB_DATE "08-03-2011"
+#define IDO2DB_DATE "08-24-2011"
 #define IDO2DB_VERSION "1.5.0-dev"
 
 #define IDO2DB_SCHEMA_VERSION "1.5.0-dev"
--- module/idoutils/src/sockdebug.c
+++ module/idoutils/src/sockdebug.c
@@ -11,7 +11,7 @@
 
 #define SOCKDEBUG_VERSION "1.5.0-dev"
 #define SOCKDEBUG_NAME "SOCKDEBUG"
-#define SOCKDEBUG_DATE "08-03-2011"
+#define SOCKDEBUG_DATE "08-24-2011"
 
 
 int cleanup_socket(int, char *);
