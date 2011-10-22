2f789eeddea6807d837e63775e3f37fa4c448ae2
Michael Friedrich <michael.friedrich@univie.ac.at>
2011-10-22 14:44:58 +0200

    make inden.sh a bit more flexible allowing multiple files to be passed (Andreas Ericsson)
    keeping indent intact, nice catchup.

--- indent.sh
+++ indent.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-FILE=$1
-
-astyle --style=java --indent=tab --unpad-paren --pad-oper --pad-header --suffix=none --brackets=linux $FILE
+astyle --style=java --indent=tab --unpad-paren --pad-oper --pad-header --suffix=none --brackets=linux "$@"
