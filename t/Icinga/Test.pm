5739a1183f38ed947bdf09beb2f978b0fc34a01f
Alexander Wirt <formorer@debian.org>
2012-02-23 23:51:01 +0100

    Add Icinga::Test lib

---
+++ t/Icinga/Test.pm
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+package Icinga::Test;
+
+use strict;
+use warnings;
+
+use Exporter 'import';
+our @EXPORT_OK = qw( run_cgi);
+
+use IPC::Run3 qw( run3 );
+
+use FindBin qw($Bin);
+
+my $topdir = "$Bin/..";
+my $cgi_dir = "$topdir/cgi";
+my $cgi = "$cgi_dir/status.cgi";
+
+sub run_cgi ($$$$) {
+    my ($config, $method, $query_string, $cgi) = @_; 
+    my @cmd = qw ( sh -c );
+    chdir $Bin or die "Cannot chdir";
+
+    my $cmd = sprintf("ICINGA_CGI_CONFIG=%s REMOTE_USER=icingaadmin REQUEST_METHOD=%s QUERY_STRING=%s %s",
+        $config,
+        $method,
+        $query_string,
+        "$cgi_dir/$cgi"
+    );
+    my @test = qw ( date );
+    my ($in, $out, $err) = '';
+    run3 ($cmd, \$in, \$out, \$err) or die "cat: $? - $! - $err";
+    return $out;
+}
+
+1;
