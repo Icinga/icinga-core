964677aeae36e031340044ed3bc012189f04691a
Alexander Wirt <formorer@grml.org>
2012-04-23 13:56:25 +0200

    I am a stupid *censored* today. really push the right paths into path

--- tools/icinga_verification.pl
+++ tools/icinga_verification.pl
@@ -361,9 +361,8 @@ sub get_key_from_ini ($$) {
 sub which (@) {
     my @binaries = @_;
     my @path = reverse( split( ':', $PATH ));
-    my $icinga_path = "$icinga_base/../bin";
-    my $icinga_path = "$icinga_base/../sbin";
-    push @path, $icinga_path;
+    push @path "$icinga_base/../bin";
+    push @path "$icinga_base/../sbin";
     print "looking for binaries in ", join(",", @path), "\n" if $verbose;
 
     foreach my $binary (@binaries) {
