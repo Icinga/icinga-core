59ec29263fe9cc2d9bcf68842bc0bed9dfac3df4
Michael Friedrich <michael.friedrich@univie.ac.at>
2012-06-12 17:58:35 +0200

    mark r1.7 as 1.7.1 release target

--- icinga.spec
+++ icinga.spec
@@ -20,7 +20,7 @@
 
 Summary: Open Source host, service and network monitoring program
 Name: icinga
-Version: 1.7.0
+Version: 1.7.1
 Release: %{revision}%{?dist}
 License: GPLv2
 Group: Applications/System
--- module/idoutils/include/common.h
+++ module/idoutils/include/common.h
@@ -22,8 +22,8 @@
 #define LOG2IDO_NAME 		"LOG2IDO"
 
 /* only one space for update-version matching */
-#define IDO_DATE "05-15-2012"
-#define IDO_VERSION "1.7.0"
+#define IDO_DATE "06-18-2012"
+#define IDO_VERSION "1.7.1"
 
 #define IDO_SCHEMA_VERSION "1.7.0"
 
