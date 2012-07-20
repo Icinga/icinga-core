f8245d883cb64f7d1b3f2b2b672b5edf9f6caf18
Michael Friedrich <michael.friedrich@univie.ac.at>
2012-07-20 12:33:50 +0200

    beautify changelog on pagination, add thanks to sources, update copyright
    refs #2725

--- cgi/cgiutils.c
+++ cgi/cgiutils.c
@@ -3,6 +3,7 @@
  * CGIUTILS.C - Common utilities for Icinga CGIs
  *
  * Copyright (c) 1999-2009 Ethan Galstad (egalstad@nagios.org)
+ * Copyright (c) 2012 Nagios Core Development Team and Community Contributors
  * Copyright (c) 2009-2012 Icinga Development Team (http://www.icinga.org)
  *
  * License:
