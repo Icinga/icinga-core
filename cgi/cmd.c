2949ee4f2e4ecceb335b4269430298233aa58ec9
Ricardo Bartels <ricardo@bitchbrothers.com>
2012-08-20 16:57:53 +0200

    classic-ui: Added "Select All" checkbox to cmd.cgi host/service list #2697
    refs: #2697
    
    * Added "Select All" checkbox
    * Added title to checkboxes (active / passive)

--- cgi/cmd.c
+++ cgi/cmd.c
@@ -813,12 +813,24 @@ void print_object_list(int list_type) {
 	printf("<tr><td colspan=\"2\">&nbsp;</td></tr>\n");
 	printf("<tr class=\"sectionHeader\"><td colspan=\"2\" >Affected Objects</td></tr>\n");
 
-	printf("<tr><td colspan=\"2\"><TABLE cellspacing='2' cellpadding='0' border='0' width='100%%'>\n");
+	printf("<tr><td colspan=\"2\">\n");
+
+	printf("<script language='javascript' type=\"text/javascript\">\nchecked=false;\n");
+	printf("function checkAllBoxes() {\n"
+		"	checked = (checked == false) ? true : false;\n"
+		"	for (var i=0; i < %d; i++) {\n"
+		"		var checkboxes = document.getElementById(\"cb_\" + i);\n"
+		"		if (checkboxes != null ) { checkboxes.checked = checked; }\n"
+		"	}\n"
+		"}\n", NUMBER_OF_STRUCTS);
+	printf("</script>\n");
+
+	printf("<TABLE cellspacing='2' cellpadding='0' border='0' width='100%%'>\n");
 
 	if (list_type == PRINT_SERVICE_LIST)
-		printf("<tr class=\"objectDescription\"><td width=\"46%%\">Host</td><td width=\"46%%\">Service</td><td width='16'></td></tr>\n");
+		printf("<tr class=\"objectDescription\"><td width=\"46%%\">Host</td><td width=\"46%%\">Service</td><td width='16'><input type='checkbox' onclick=\"checkAllBoxes();\" title=\"Check All\"></td></tr>\n");
 	else if (list_type == PRINT_HOST_LIST)
-		printf("<tr class=\"objectDescription\"><td colspan=\"2\" width=\"96%%\">Hosts</td><td width='16'></td></tr>\n");
+		printf("<tr class=\"objectDescription\"><td colspan=\"2\" width=\"96%%\">Hosts</td><td width='16'><input type='checkbox' onclick=\"checkAllBoxes();\" title=\"Check All\"></td></tr>\n");
 	else
 		printf("<tr><td colspan=\"3\">&nbsp;</td></tr>\n");
 
@@ -831,6 +843,9 @@ void print_object_list(int list_type) {
 			if (commands[x].host_name == NULL)
 				continue;
 
+			if (list_type == PRINT_SERVICE_LIST && commands[x].description == NULL)
+				continue;
+
 			if (strlen(commands[x].host_name) != 0 && (
 				command_type == CMD_SCHEDULE_HOST_CHECK ||
 				command_type == CMD_DISABLE_HOST_CHECK ||
@@ -840,7 +855,6 @@ void print_object_list(int list_type) {
 					if (temp_hoststatus->checks_enabled == FALSE)
 						host_passive = TRUE;
 				}
-
 
 				if (list_type == PRINT_SERVICE_LIST && strlen(commands[x].description) != 0 ) {
 					if((temp_servicestatus = find_servicestatus(commands[x].host_name, commands[x].description)) != NULL) {
@@ -862,7 +876,7 @@ void print_object_list(int list_type) {
 			/* hostname and service description are present */
 			if (strlen(commands[x].host_name) != 0  && strlen(commands[x].description) != 0) {
 				printf(">%s</td><td>%s</td>", escape_string(commands[x].host_name), escape_string(commands[x].description));
-				printf("<td align='center'><input type='checkbox' name='hostservice' value='%s^%s' %s></td></tr>\n", escape_string(commands[x].host_name), escape_string(commands[x].description), (service_passive == FALSE) ? "checked" : "");
+				printf("<td align='center'><input type='checkbox' name='hostservice' id=\"cb_%d\" value='%s^%s' title=\"%s Service\" %s></td></tr>\n", x, escape_string(commands[x].host_name), escape_string(commands[x].description), (service_passive == FALSE) ? "Active" : "Passive", (service_passive == FALSE) ? "checked" : "");
 			} else {
 				/* if hostname is empty print inputbox instead */
 				if (!strcmp(commands[x].host_name, ""))
@@ -883,7 +897,7 @@ void print_object_list(int list_type) {
 				printf(" style=\"font-weight:bold;\">Host:</td><td><INPUT TYPE='TEXT' NAME='host' SIZE=30></td><td></td></tr>\n");
 			else {
 				printf(" style=\"font-weight:bold;\">Host:</td><td>%s</td>", escape_string(commands[x].host_name));
-				printf("<td align='center'><input type='checkbox' name='host' value='%s' %s></td></tr>\n", escape_string(commands[x].host_name), (host_passive == FALSE) ? "checked" : "");
+				printf("<td align='center'><input type='checkbox' name='host' id=\"cb_%d\" value='%s' title=\"%s Host\" %s></td></tr>\n", x, escape_string(commands[x].host_name), (host_passive == FALSE) ? "Active" : "Passive", (host_passive == FALSE) ? "checked" : "");
 			}
 		} else if (list_type == PRINT_COMMENT_LIST) {
 			printf(" style=\"font-weight:bold;\">Comment ID:</td><td><INPUT TYPE='HIDDEN' NAME='com_id' VALUE='%lu'>%lu</td></tr>\n", multi_ids[x], multi_ids[x]);
@@ -892,7 +906,7 @@ void print_object_list(int list_type) {
 		}
 	}
 
-	printf("</td><tr></table>\n");
+	printf("</td><tr></table>\n</td></tr>\n");
 
 	return;
 }
