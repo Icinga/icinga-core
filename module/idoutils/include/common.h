201d4a57430593ebc13d73edd347744a5759f850
Michael Friedrich <michael.friedrich@netways.de>
2012-12-12 09:11:17 +0100

    bump to 1.8.3

--- module/idoutils/include/common.h
+++ module/idoutils/include/common.h
@@ -22,8 +22,8 @@
 #define LOG2IDO_NAME 		"LOG2IDO"
 
 /* only one space for update-version matching */
-#define IDO_DATE "11-28-2012"
-#define IDO_VERSION "1.8.2"
+#define IDO_DATE "12-12-2012"
+#define IDO_VERSION "1.8.3"
 
 #define IDO_SCHEMA_VERSION "1.8.0"
 
