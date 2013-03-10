5074e8d9302134d0e089d43015616fe07492b86f
Michael Friedrich <michael.friedrich@netways.de>
2013-03-10 14:30:46 +0100

    core: fix host_check, last_check == next_check wrong in scheduling queue
    this is one of those side effects when detecting orphaned checks, so in
    order to solve the issue, remove workaround for the next_check of on
    demand hosts, and look our if they occur on the orphaned checks again.
    
    fixes #2195

--- base/checks.c
+++ base/checks.c
@@ -3239,11 +3239,6 @@ int run_async_host_check_3x(host *hst, int check_options, double latency, int sc
 	/* get the command start time */
 	gettimeofday(&start_time, NULL);
 
-	/* set check time for on-demand checks, so they're not incorrectly detected as being orphaned - Luke Ross 5/16/08 */
-	/* NOTE: 06/23/08 EG not sure if there will be side effects to this or not.... */
-	if (scheduled_check == FALSE)
-		hst->next_check = start_time.tv_sec;
-
 	/* increment number of host checks that are currently running... */
 	currently_running_host_checks++;
 
