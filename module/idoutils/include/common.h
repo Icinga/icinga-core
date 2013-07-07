12a21b7fa580757a02021a40246b53e73de0c78f
Michael Friedrich <michael.friedrich@netways.de>
2013-07-07 17:50:50 +0200

    update to 1.9.3

--- module/idoutils/include/common.h
+++ module/idoutils/include/common.h
@@ -22,8 +22,8 @@
 #define LOG2IDO_NAME 		"LOG2IDO"
 
 /* only one space for update-version matching */
-#define IDO_DATE "06-30-2013"
-#define IDO_VERSION "1.9.2"
+#define IDO_DATE "07-07-2013"
+#define IDO_VERSION "1.9.3"
 
 #define IDO_SCHEMA_VERSION "1.9.0"
 
