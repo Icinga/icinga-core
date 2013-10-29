dc3d214bc75d174f06c415396007d775eace37e1
Michael Friedrich <michael.friedrich@netways.de>
2013-10-29 23:10:57 +0100

    classic ui: fix context help on mouseover in cmd.cgi (thx Marc-Christian Petersen)
    fixes #4971

--- cgi/cmd.c
+++ cgi/cmd.c
@@ -944,7 +944,7 @@ void print_object_list(int list_type) {
 
 void print_help_box(char *content) {
 
-	printf("<img src='%s%s' onMouseOver=\"return tooltip('<table border='0' width='100%%' height='100%%'>", url_images_path, CONTEXT_HELP_ICON);
+	printf("<img src='%s%s' onMouseOver=\"return tooltip('<table border=0 width=100%% height=100%%>", url_images_path, CONTEXT_HELP_ICON);
 	printf("<tr><td>%s</td></tr>", content);
 	printf("</table>', '&nbsp;&nbsp;&nbsp;Help', 'border:1, width:500, xoffset:-250, yoffset:25, bordercolor:#333399, title_padding:2px, titletextcolor:#FFFFFF, backcolor:#CCCCFF');\" onMouseOut=\"return hideTip()\"");
 	printf(" border='0'>");
