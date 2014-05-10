a84b72ff40b104c7a84eb012d69f5b6fd612e430
Michael Friedrich <Michael.Friedrich@netways.de>
2014-05-10 20:49:26 +0200

    IDOUtils: Add is_reachable columns for {host,service}status tables.

--- module/idoutils/db/mysql/upgrade/mysql-upgrade-1.11.3.sql
+++ module/idoutils/db/mysql/upgrade/mysql-upgrade-1.11.3.sql
@@ -16,6 +16,10 @@ ALTER TABLE icinga_servicegroups ADD COLUMN notes TEXT character set latin1  def
 ALTER TABLE icinga_servicegroups ADD COLUMN notes_url TEXT character set latin1  default NULL;
 ALTER TABLE icinga_servicegroups ADD COLUMN action_url TEXT character set latin1  default NULL;
 
+ALTER TABLE icinga_hoststatus ADD COLUMN is_reachable smallint(6) DEFAULT NULL;
+ALTER TABLE icinga_servicestatus ADD COLUMN is_reachable smallint(6) DEFAULT NULL;
+
+
 -- -----------------------------------------
 -- update dbversion
 -- -----------------------------------------
--- module/idoutils/db/pgsql/upgrade/pgsql-upgrade-1.11.3.sql
+++ module/idoutils/db/pgsql/upgrade/pgsql-upgrade-1.11.3.sql
@@ -17,6 +17,9 @@ ALTER TABLE icinga_servicegroups ADD COLUMN notes TEXT default NULL;
 ALTER TABLE icinga_servicegroups ADD COLUMN notes_url TEXT default NULL;
 ALTER TABLE icinga_servicegroups ADD COLUMN action_url TEXT default NULL;
 
+ALTER TABLE icinga_hoststatus ADD COLUMN is_reachable smallint(6) DEFAULT NULL;
+ALTER TABLE icinga_servicestatus ADD COLUMN is_reachable smallint(6) DEFAULT NULL;
+
 -- -----------------------------------------
 -- update dbversion
 -- -----------------------------------------
