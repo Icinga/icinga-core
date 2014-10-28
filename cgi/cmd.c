a52dc39967aeac186ac50570c952f59764355820
Carl R. Friend <crfriend@rcn.com>
2014-10-28 19:37:47 -0400

    Fix QA problem with "config-option-for-sticky-ack-6237"
    This commit fixes a QA-related problem which could make the UI
    display confusing things when trying to alter the "Sticky Acknowledgment"
    tick-box.
    
    The correct merge sequence for this is to first apply revision 1505fd78
    and then immediately apply this one as this one fixes *just* the behaviour
    problems.  Just applying this commit will miss infrastructure changes
    that were necessary to define the desired behaviours.

--- cgi/cmd.c
+++ cgi/cmd.c
@@ -631,8 +631,8 @@ int process_cgivars(void) {
 			send_notification = TRUE;
 
 		/* we got the acknowledgement type */
-		else if (strcmp(key, "sticky_ack") == 0 || strcmp(key, "set_sticky_acknowledgment") == 0)
-			sticky_ack_set = set_sticky_acknowledgment = TRUE;
+		else if (!strcmp(key, "sticky_ack"))
+			sticky_ack_set = TRUE;
 
 		/* we use the end_time as expire time */
 		else if (!strcmp(key, "use_ack_end_time"))
@@ -1003,7 +1003,7 @@ void print_form_element(int element, int cmd) {
 /*
 		printf("<input type='checkbox' name='sticky_ack' %s></td></tr>\n", (sticky_ack == TRUE) ? "CHECKED" : "");
 */
-		printf("<input type='checkbox' id='sticky_ack_checkbox' name='sticky_ack' onClick=\"if (document.getElementById('sticky_ack_checkbox').checked == true) document.getElementById('sticky_ack_row').style.display = ''; else document.getElementById('sticky_ack_row').style.display = 'none';\" %s></td></tr>\n", (set_sticky_acknowledgment == TRUE) ? "CHECKED" : "");
+		printf("<input type='checkbox' id='sticky_ack_checkbox' name='sticky_ack' %s></td></tr>\n", (set_sticky_acknowledgment == TRUE) ? "CHECKED" : "");
 		break;
 
 	case PRINT_SEND_NOTFICATION:
