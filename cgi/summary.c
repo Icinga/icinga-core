Icinga/icinga-core

commit a4f01c32a4cfc04430476741e0cbaa30a80310ee

classic-ui: fixed possible NULL dereference-bug in summary.c #3740
refs: #3740

whatthecommit: Become a programmer, they said. It'll be fun, they said.

cgi/summary.c
@@ -1682,10 +1682,12 @@ void display_recent_alerts(void) {
 			odd = 1;
 
 		/* find the host */
-		temp_host = find_host(temp_event->host_name);
+		if ((temp_host = find_host(temp_event->host_name)) == NULL)
+			continue;
 
 		/* find the service */
-		temp_service = find_service(temp_event->host_name, temp_event->service_description);
+		if ((temp_service = find_service(temp_event->host_name, temp_event->service_description)) == NULL)
+			continue;
 
 		get_time_string(&temp_event->time_stamp, date_time, (int)sizeof(date_time), SHORT_DATE_TIME);
 
@@ -1991,7 +1993,7 @@ void display_top_alerts(void) {
 			printf(" \"host_name\": \"%s\", ", json_encode(temp_producer->host_name));
 
 			temp_host = find_host(temp_producer->host_name);
-			printf("\"host_display_name\": \"%s\", ", (temp_host != NULL && temp_host->display_name != NULL) ? json_encode(temp_host->display_name) : json_encode(temp_host->name));
+			printf("\"host_display_name\": \"%s\", ", (temp_host != NULL && temp_host->display_name != NULL) ? json_encode(temp_host->display_name) : json_encode(temp_producer->host_name));
 			if (temp_producer->producer_type == AE_HOST_PRODUCER) {
 				printf(" \"service_description\": null, ");
 				printf(" \"service_display_name\": null, ");
@@ -1999,7 +2001,7 @@ void display_top_alerts(void) {
 				printf(" \"service_description\": \"%s\", ", json_encode(temp_producer->service_description));
 
 				temp_service = find_service(temp_producer->host_name, temp_producer->service_description);
-				printf("\"service_display_name\": \"%s\", ", (temp_service != NULL && temp_service->display_name != NULL) ? json_encode(temp_service->display_name) : json_encode(temp_service->description));
+				printf("\"service_display_name\": \"%s\", ", (temp_service != NULL && temp_service->display_name != NULL) ? json_encode(temp_service->display_name) : json_encode(temp_producer->service_description));
 			}
 			printf(" \"total_alerts\": %d}", temp_producer->total_alerts);
 		} else if (content_type == CSV_CONTENT) {

cgi/histogram.c
@@ -352,9 +352,9 @@ int main(int argc, char **argv) {
 
 			printf("<DIV ALIGN=CENTER CLASS='dataTitle'>\n");
 			if (display_type == DISPLAY_HOST_HISTOGRAM)
-				printf("Host '%s'", (temp_host->display_name != NULL) ? temp_host->display_name : temp_host->name);
+				printf("Host '%s'", (temp_host != NULL && temp_host->display_name != NULL) ? temp_host->display_name : host_name);
 			else if (display_type == DISPLAY_SERVICE_HISTOGRAM)
-				printf("Service '%s' On Host '%s'", (temp_service->display_name != NULL) ? temp_service->display_name : temp_service->description, (temp_host->display_name != NULL) ? temp_host->display_name : temp_host->name);
+				printf("Service '%s' On Host '%s'", (temp_service != NULL && temp_service->display_name != NULL) ? temp_service->display_name : service_desc, (temp_host != NULL && temp_host->display_name != NULL) ? temp_host->display_name : host_name);
 			printf("</DIV>\n");
 
 			printf("<BR>\n");
@@ -479,11 +479,11 @@ int main(int argc, char **argv) {
 	/* check authorization... */
 	if (display_type == DISPLAY_HOST_HISTOGRAM) {
 		temp_host = find_host(host_name);
-		if (temp_host == NULL || is_authorized_for_host(temp_host, &current_authdata) == FALSE)
+		if (is_authorized_for_host(temp_host, &current_authdata) == FALSE)
 			is_authorized = FALSE;
 	} else if (display_type == DISPLAY_SERVICE_HISTOGRAM) {
 		temp_service = find_service(host_name, service_desc);
-		if (temp_service == NULL || is_authorized_for_service(temp_service, &current_authdata) == FALSE)
+		if (is_authorized_for_service(temp_service, &current_authdata) == FALSE)
 			is_authorized = FALSE;
 	}
 	if (is_authorized == FALSE) {
@@ -683,7 +683,7 @@ int main(int argc, char **argv) {
 			for (temp_service = service_list; temp_service != NULL; temp_service = temp_service->next) {
 				if (is_authorized_for_service(temp_service, &current_authdata) == TRUE)
 					temp_host = find_host(temp_service->host_name);
-				printf("<option value='%s^%s'>%s;%s\n", escape_string(temp_service->host_name), escape_string(temp_service->description), (temp_host->display_name != NULL) ? temp_host->display_name : temp_host->name, (temp_service->display_name != NULL) ? temp_service->display_name : temp_service->description);
+				printf("<option value='%s^%s'>%s;%s\n", escape_string(temp_service->host_name), escape_string(temp_service->description), (temp_host != NULL && temp_host->display_name != NULL) ? temp_host->display_name : temp_service->host_name, (temp_service->display_name != NULL) ? temp_service->display_name : temp_service->description);
 			}
 
 			printf("</select>\n");
@@ -1572,9 +1572,9 @@ void graph_all_histogram_data(void) {
 	end_time[strlen(end_time)-1] = '\x0';
 
 	if (display_type == DISPLAY_HOST_HISTOGRAM)
-		snprintf(temp_buffer, sizeof(temp_buffer) - 1, "Event History For Host '%s'", (temp_host->display_name != NULL) ? temp_host->display_name : temp_host->name);
+		snprintf(temp_buffer, sizeof(temp_buffer) - 1, "Event History For Host '%s'", (temp_host != NULL && temp_host->display_name != NULL) ? temp_host->display_name : host_name);
 	else
-		snprintf(temp_buffer, sizeof(temp_buffer) - 1, "Event History For Service '%s' On Host '%s'", (temp_service->display_name != NULL) ? temp_service->display_name : temp_service->description, (temp_host->display_name != NULL) ? temp_host->display_name : temp_host->name);
+		snprintf(temp_buffer, sizeof(temp_buffer) - 1, "Event History For Service '%s' On Host '%s'", (temp_service != NULL && temp_service->display_name != NULL) ? temp_service->display_name : service_desc, (temp_host != NULL && temp_host->display_name != NULL) ? temp_host->display_name : host_name);
 	temp_buffer[sizeof(temp_buffer)-1] = '\x0';
 	string_width = gdFontSmall->w * strlen(temp_buffer);
 	gdImageString(histogram_image, gdFontSmall, DRAWING_X_OFFSET + (DRAWING_WIDTH / 2) - (string_width / 2), 0, (unsigned char *)temp_buffer, color_black);

cgi/status.c
@@ -1022,6 +1022,9 @@ int main(void) {
 		/* get the host status information */
 		temp_hoststatus = find_hoststatus(temp_service->host_name);
 
+		if (temp_hoststatus == NULL)
+			continue;
+
 		/* check host properties filter */
 		if (passes_host_properties_filter(temp_hoststatus) == FALSE)
 			continue;
@@ -1037,7 +1040,7 @@ int main(void) {
 		if (display_type == DISPLAY_HOSTS && show_all_hosts == FALSE && search_string == NULL) {
 			found = FALSE;
 			for (i = 0; req_hosts[i].entry != NULL; i++) {
-				if (!strcmp(req_hosts[i].entry, temp_hoststatus->host_name) || !strcmp(req_hosts[i].entry, temp_host->display_name)) {
+				if (!strcmp(req_hosts[i].entry, temp_hoststatus->host_name) || (temp_host != NULL && !strcmp(req_hosts[i].entry, temp_host->display_name))) {
 					found = TRUE;
 					break;
 				}
@@ -1072,7 +1075,7 @@ int main(void) {
 			temp_hoststatus->added |= STATUS_COUNTED_UNFILTERED;
 		}
 
-		/* see if we should display services for hosts with tis type of status */
+		/* see if we should display services for hosts with this type of status */
 		if (!(host_status_types & temp_hoststatus->status))
 			continue;
 
@@ -2348,6 +2351,9 @@ void show_service_detail(void) {
 
 		/* find the host */
 		temp_host = find_host(temp_status->host_name);
+
+		if (temp_host == NULL)
+			continue;
 
 		/* find the service  */
 		temp_service = find_service(temp_status->host_name, temp_status->svc_description);

cgi/trends.c
@@ -471,9 +471,9 @@ int main(int argc, char **argv) {
 
 			printf("<DIV ALIGN=CENTER CLASS='dataTitle'>\n");
 			if (display_type == DISPLAY_HOST_TRENDS)
-				printf("Host '%s'", (temp_host->display_name != NULL) ? temp_host->display_name : temp_host->name);
+				printf("Host '%s'", (temp_host != NULL && temp_host->display_name != NULL) ? temp_host->display_name : host_name);
 			else if (display_type == DISPLAY_SERVICE_TRENDS)
-				printf("Service '%s' On Host '%s'", (temp_service->display_name != NULL) ? temp_service->display_name : temp_service->description, (temp_host->display_name != NULL) ? temp_host->display_name : temp_host->name);
+				printf("Service '%s' On Host '%s'", (temp_service != NULL && temp_service->display_name != NULL) ? temp_service->display_name : service_desc, (temp_host != NULL && temp_host->display_name != NULL) ? temp_host->display_name : host_name);
 			printf("</DIV>\n");
 
 			printf("<BR>\n");
@@ -599,11 +599,11 @@ int main(int argc, char **argv) {
 	/* check authorization... */
 	if (display_type == DISPLAY_HOST_TRENDS) {
 		temp_host = find_host(host_name);
-		if (temp_host == NULL || is_authorized_for_host(temp_host, &current_authdata) == FALSE)
+		if (is_authorized_for_host(temp_host, &current_authdata) == FALSE)
 			is_authorized = FALSE;
 	} else if (display_type == DISPLAY_SERVICE_TRENDS) {
 		temp_service = find_service(host_name, service_desc);
-		if (temp_service == NULL || is_authorized_for_service(temp_service, &current_authdata) == FALSE)
+		if (is_authorized_for_service(temp_service, &current_authdata) == FALSE)
 			is_authorized = FALSE;
 	}
 	if (is_authorized == FALSE) {

commit c02679a37b59b50099599ee8a635079fa792d5a3

List host alerts in Most Recent Alerts report. Fix #1599.
In add_archived_event(), service_description is set to NULL for events
of type AE_HOST_ALERT. Later on in display_recent_alerts(), all events
without service_description are skipped. But it's ok for host alerts
to have no related service. We're even prepared to write "N/A" to
the reports service column. So skip events only if they're actually
a service alert.

Seems to be a regression introduced with commit a4f01c32a4cfc04430476741e0cbaa30a80310ee.

cgi/summary.c
@@ -1618,7 +1618,7 @@ void display_recent_alerts(void) {
 			continue;
 
 		/* find the service */
-		if ((temp_service = find_service(temp_event->host_name, temp_event->service_description)) == NULL)
+		if (temp_event->event_type != AE_HOST_ALERT && (temp_service = find_service(temp_event->host_name, temp_event->service_description)) == NULL)
 			continue;
 
 		get_time_string(&temp_event->time_stamp, date_time, (int)sizeof(date_time), SHORT_DATE_TIME);